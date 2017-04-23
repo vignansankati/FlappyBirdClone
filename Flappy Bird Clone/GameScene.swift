e0f199de0bef2d3f2a1c55c77f49dc9437039f4d
Vignan Sankati <vignan.nani@gmail.com>
2017-04-22 22:37:30 -0400

    Added background and animation to the background.

--- Flappy Bird Clone/GameScene.swift
+++ Flappy Bird Clone/GameScene.swift
@@ -13,8 +13,26 @@ class GameScene: SKScene {
     
     // created a bird node
     var bird = SKSpriteNode()
+    var backgroundImage = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        
+        let backTexture = SKTexture(imageNamed: "bg.png")
+        
+        let moveBackAnimation = SKAction.move(by: CGVector(dx: -backTexture.size().width, dy: 0.0), duration: 5)
+        let shiftBackAnimation = SKAction.move(by: CGVector(dx: backTexture.size().width, dy: 0.0), duration: 0)
+        let backMovement = SKAction.repeatForever(SKAction.sequence([moveBackAnimation, shiftBackAnimation]))
+        
+        for i in 0 ..< 3  {
+            backgroundImage = SKSpriteNode(texture: backTexture)
+            backgroundImage.position = CGPoint(x: backTexture.size().width * CGFloat(i), y: self.frame.midY)
+            backgroundImage.size.height = self.frame.height
+            backgroundImage.run(backMovement)
+            self.addChild(backgroundImage)
+            backgroundImage.zPosition = -1
+        }
+        
+        
         //created a sprite kit texture
         let flappyBirdTexture = SKTexture(imageNamed: "flappy1.png")
         let flappyBirdFlyingTexture = SKTexture(imageNamed: "flappy2.png")
@@ -27,8 +45,10 @@ class GameScene: SKScene {
         //positioning the bird node created
         bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         bird.run(birdFlap)
+        
         //adding the bird to the viewcontroller
         self.addChild(bird)
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
